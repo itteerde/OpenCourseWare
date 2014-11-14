b98e4015a0712b6f0b1dce18f39e48117d1ac33d
Erik Itter <erik.itter@gmail.com>
2014-11-14 01:51:58 +0100

    p. 50, iterative fibonacci numbers

--- Computer Science/Programming Languages/Scheme/Structure and Interpretation of Computer Programs/sicp.rkt
+++ Computer Science/Programming Languages/Scheme/Structure and Interpretation of Computer Programs/sicp.rkt
@@ -41,3 +41,11 @@
         ((= x 0) (* 2 y))
         ((= y 1) 2)
         (else (ackermann (- x 1) (ackermann x (- y 1))))))
+
+(define (fib n)
+  (fib-iter 1 0 n))
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+
