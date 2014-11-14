0df1bbba59b23b0904e5d6fa53b2935c9d9e1da7
Erik Itter <erik.itter@gmail.com>
2014-11-14 01:30:42 +0100

    p. 43, linear iterative process

--- Computer Science/Programming Languages/Scheme/Structure and Interpretation of Computer Programs/sicp.rkt
+++ Computer Science/Programming Languages/Scheme/Structure and Interpretation of Computer Programs/sicp.rkt
@@ -23,3 +23,11 @@
 
 (define (average x y)
   (/ (+ x y) 2))
+
+(define (factorial n)
+  (define (iter product counter)
+    (if (> counter n)
+        product
+        (iter (* counter product)
+              (+ counter 1))))
+  (iter 1 1))
