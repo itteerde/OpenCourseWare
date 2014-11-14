ad234dbcee585904d19fa65c4d94a71d2f7afaac
Erik Itter <erik.itter@gmail.com>
2014-11-14 01:16:25 +0100

    p. 30, simple function composition (iterative sqrt approximation)

--- Computer Science/Programming Languages/Scheme/Structure and Interpretation of Computer Programs/sicp.rkt
+++ Computer Science/Programming Languages/Scheme/Structure and Interpretation of Computer Programs/sicp.rkt
@@ -9,3 +9,17 @@
 (define (circumference_circle radius) (* 2 pi radius))
 
 (define (square x) (* x x))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (average guess(/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.0000001))
+
+(define (average x y)
+  (/ (+ x y) 2))
