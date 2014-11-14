cfac5558b993059febd93b19382b940c14247409
git <erik.itter@gmail.com>
2014-11-15 00:21:35 +0100

    p. 199, symbolic derivate

--- Computer Science/Programming Languages/Scheme/Structure and Interpretation of Computer Programs/sicp.rkt
+++ Computer Science/Programming Languages/Scheme/Structure and Interpretation of Computer Programs/sicp.rkt
@@ -203,3 +203,57 @@
                  (map (lambda (p) (cons x p))
                       (permutations (remove x s))))
                s)))
+
+; p. 171 maximum queen puzzle
+;(define (queen board-size)
+;  (define (queen-cols k)
+;    (if (= k 0)
+;        (list empty-board)
+;        (filter
+;         (lambda (positions) (safe? k positions))
+;         (flatmap
+;          (lambda (rest-of-queens)
+;            (map (lambda (new-row)
+;                   (adjoin-position
+;                    new-row k rest-of-queens))
+;                 (enumerate-interval 1 board-size)))
+;          (queen-cols (- k 1))))))
+;  (queen-cols board size))
+
+; p. 199
+(define (derivate exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ((sum? exp) (make-sum (derivate (addend exp) var)
+                              (derivate (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (derivate (multiplicand exp) var))
+          (make-product (derivate (multiplier exp) var)
+                        (multiplicand exp))))
+        (else
+         (error "unknown expression type: DERIVATE" exp))))
+
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2))
+         (+ a1 a2))
+        (else (list '+ a1 a2))))
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+(define (sum? x) (and (pair? x) (eq? (car x) '+)))
+(define (addend s) (cadr s))
+(define (augend s) (caddr s))
+(define (product? x) (and (pair? x) (eq? (car x) '*)))
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+(define (=number? exp num) (and (number? exp) (= exp num)))
