639e3696231e2c040f5f9a749bb518dd83def164
git <erik.itter@gmail.com>
2014-11-14 02:52:59 +0100

    p. 66, (prime? n)

--- Computer Science/Programming Languages/Scheme/Structure and Interpretation of Computer Programs/sicp.rkt
+++ Computer Science/Programming Languages/Scheme/Structure and Interpretation of Computer Programs/sicp.rkt
@@ -49,3 +49,14 @@
       b
       (fib-iter (+ a b) a (- count 1))))
 
+
+(define (remainder x y) (mod x y))
+
+(define (smallest-divisor n) (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b) (= (remainder b a) 0))
+(define (prime? n)
+  (= n (smallest-divisor n)))
