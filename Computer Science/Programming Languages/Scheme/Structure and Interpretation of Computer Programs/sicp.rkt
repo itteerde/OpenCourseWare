fd08641fc84e100435a93ab7fa7fff205a0353c8
git <erik.itter@gmail.com>
2014-11-14 11:45:43 +0100

    chapter 1 pretty much complete except excersices

--- Computer Science/Programming Languages/Scheme/Structure and Interpretation of Computer Programs/sicp.rkt
+++ Computer Science/Programming Languages/Scheme/Structure and Interpretation of Computer Programs/sicp.rkt
@@ -138,3 +138,12 @@
 (define (deriv g)
   (lambda (x) (/ (- (g (+ x dx)) (g x)) dx)))
 (define dx 0.000001)
+
+(define (newton-transform g)
+  (lambda (x) (- x (/ (g x) ((deriv g) x)))))
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+; p. 101
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess))
