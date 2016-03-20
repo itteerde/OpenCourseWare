9ee0afacc7eac495f7292fda5dfaba5c426edb25
git <erik.itter@gmail.com>
2016-03-20 21:05:41 +0100

    Project Euler, Problem 60, brute force fail

--- Computer Science/Programming Languages/Java/Project Euler/Problem0060.java
+++ Computer Science/Programming Languages/Java/Project Euler/Problem0060.java
@@ -10,86 +10,90 @@ public class Problem0060 {
 	private static final Logger LOGGER = Logger.getLogger(Problem0060.class.getName());
 
 	public static void main(String[] args) {
-		Vector<Integer> primes = SiebErastosthenes.primes(100000000);
+		Vector<Integer> primeVector = SiebErastosthenes.primes(10000000);
+		int[] primes = new int[primeVector.size()];
+		for (int i = 0; i < primes.length; i++) {
+			primes[i] = primeVector.elementAt(i);
+		}
+		primeVector = null;
 
-		System.out.println("biggest prime in data: " + primes.elementAt(primes.size() - 1));
-		System.out.println("#primes in data: " + primes.size());
+		System.out.println("biggest prime in data: " + primes[primes.length - 1]);
+		System.out.println("#primes in data: " + primes.length);
 
-		int maxPrime = 10000;
+		int maxPrime = 1200;// 4000
+		int minSum4 = Integer.MAX_VALUE;
+		int minSum5 = Integer.MAX_VALUE;
 
 		for (int i = 1; i < maxPrime; i++) {
 
-			if (i % (maxPrime / 10) == 0) {
-				LOGGER.info("i: " + i);
-			}
-
 			for (int j = 1; j < maxPrime; j++) {
-				if (i == j || !SiebErastosthenes.isPrime((int) concatenate(primes.elementAt(i), primes.elementAt(j)))
-						|| !SiebErastosthenes.isPrime((int) concatenate(primes.elementAt(j), primes.elementAt(i)))) {
-					break;
+				if (i == j || !SiebErastosthenes.isPrime((int) concatenate(primes[i], primes[j]))
+						|| !SiebErastosthenes.isPrime((int) concatenate(primes[j], primes[i]))) {
+					continue;
 				}
-				for (int k = 1; k < maxPrime; k++) {
-					if (i == k || j == k
-							|| !SiebErastosthenes.isPrime((int) concatenate(primes.elementAt(i), primes.elementAt(k)))
-							|| !SiebErastosthenes
-									.isPrime((int) concatenate(primes.elementAt(k), primes.elementAt(i)))) {
-						break;
+				for (int k = j + 1; k < maxPrime; k++) {
+					if (i == k || j == k || !SiebErastosthenes.isPrime((int) concatenate(primes[i], primes[k]))
+							|| !SiebErastosthenes.isPrime((int) concatenate(primes[k], primes[i]))) {
+						continue;
 
 					}
-					if (!SiebErastosthenes.isPrime((int) concatenate(primes.elementAt(j), primes.elementAt(k)))
-							|| !SiebErastosthenes
-									.isPrime((int) concatenate(primes.elementAt(k), primes.elementAt(j)))) {
-						break;
+					if (!SiebErastosthenes.isPrime((int) concatenate(primes[j], primes[k]))
+							|| !SiebErastosthenes.isPrime((int) concatenate(primes[k], primes[j]))) {
+						continue;
 
 					}
-					for (int l = 1; l < maxPrime; l++) {
+					for (int l = k + 1; l < maxPrime; l++) {
 						if (i == l || j == l || k == l
-								|| !SiebErastosthenes
-										.isPrime((int) concatenate(primes.elementAt(i), primes.elementAt(l)))
-								|| !SiebErastosthenes
-										.isPrime((int) concatenate(primes.elementAt(l), primes.elementAt(i)))) {
-							break;
+								|| !SiebErastosthenes.isPrime((int) concatenate(primes[i], primes[l]))
+								|| !SiebErastosthenes.isPrime((int) concatenate(primes[l], primes[i]))) {
+							continue;
 
 						}
-						if (!SiebErastosthenes.isPrime((int) concatenate(primes.elementAt(j), primes.elementAt(l)))
-								|| !SiebErastosthenes
-										.isPrime((int) concatenate(primes.elementAt(l), primes.elementAt(j)))) {
-							break;
+						if (!SiebErastosthenes.isPrime((int) concatenate(primes[j], primes[l]))
+								|| !SiebErastosthenes.isPrime((int) concatenate(primes[l], primes[j]))) {
+							continue;
 
 						}
-						if (!SiebErastosthenes.isPrime((int) concatenate(k, l)) || !SiebErastosthenes
-								.isPrime((int) concatenate(primes.elementAt(l), primes.elementAt(k)))) {
-							break;
+						if (!SiebErastosthenes.isPrime((int) concatenate(primes[k], primes[l]))
+								|| !SiebErastosthenes.isPrime((int) concatenate(primes[l], primes[k]))) {
+							continue;
 
 						}
-						for (int m = 1; m < maxPrime; m++) {
+
+						int sum = primes[i] + primes[j] + primes[k] + primes[l];
+						if (sum < minSum4) {
+							minSum4 = sum;
+							System.out.println(primes[i] + "+" + primes[j] + "+" + primes[k] + "+" + primes[l] + "+"
+									+ " = " + sum);
+
+						}
+
+						for (int m = l + 1; m < maxPrime; m++) {
+
 							if (i == m || j == m || k == m || l == m
-									|| !SiebErastosthenes
-											.isPrime((int) concatenate(primes.elementAt(i), primes.elementAt(m)))
-									|| !SiebErastosthenes
-											.isPrime((int) concatenate(primes.elementAt(m), primes.elementAt(i)))) {
-								break;
+									|| !SiebErastosthenes.isPrime((int) concatenate(primes[i], primes[m]))
+									|| !SiebErastosthenes.isPrime((int) concatenate(primes[m], primes[i]))) {
+								continue;
 							}
-							if (!SiebErastosthenes.isPrime((int) concatenate(primes.elementAt(j), primes.elementAt(m)))
-									|| !SiebErastosthenes
-											.isPrime((int) concatenate(primes.elementAt(m), primes.elementAt(j)))) {
-								break;
+							if (!SiebErastosthenes.isPrime((int) concatenate(primes[j], primes[m]))
+									|| !SiebErastosthenes.isPrime((int) concatenate(primes[m], primes[j]))) {
+								continue;
 							}
-							if (!SiebErastosthenes.isPrime((int) concatenate(primes.elementAt(k), primes.elementAt(m)))
-									|| !SiebErastosthenes
-											.isPrime((int) concatenate(primes.elementAt(m), primes.elementAt(k)))) {
-								break;
+							if (!SiebErastosthenes.isPrime((int) concatenate(primes[k], primes[m]))
+									|| !SiebErastosthenes.isPrime((int) concatenate(primes[m], primes[k]))) {
+								continue;
 							}
-							if (!SiebErastosthenes.isPrime((int) concatenate(primes.elementAt(l), primes.elementAt(m)))
-									|| !SiebErastosthenes
-											.isPrime((int) concatenate(primes.elementAt(m), primes.elementAt(l)))) {
-								break;
+							if (!SiebErastosthenes.isPrime((int) concatenate(primes[l], primes[m]))
+									|| !SiebErastosthenes.isPrime((int) concatenate(primes[m], primes[l]))) {
+								continue;
 							}
 
-							System.out.println(primes.elementAt(i) + "+" + primes.elementAt(j) + "+"
-									+ primes.elementAt(k) + "+" + primes.elementAt(l) + "+" + primes.elementAt(m)
-									+ " = " + (primes.elementAt(i) + primes.elementAt(j) + primes.elementAt(k)
-											+ primes.elementAt(l) + primes.elementAt(m)));
+							sum = primes[i] + primes[j] + primes[k] + primes[l] + primes[m];
+							if (sum < minSum5) {
+								minSum5 = sum;
+								System.out.println(primes[i] + "+" + primes[j] + "+" + primes[k] + "+" + primes[l] + "+"
+										+ primes[m] + " = " + sum);
+							}
 
 						}
 					}
